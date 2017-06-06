61697dd85be0620e75c3252af7704d699ede398c
Cassio Matos <cms.usp@gmail.com>
2017-06-06 00:38:24 -0300

    Estrutura base do projeto

---
+++ main.lua
@@ -0,0 +1,17 @@
+local composer = require( "composer" )
+
+-- Hide status bar
+display.setStatusBar( display.HiddenStatusBar )
+
+-- Seed the random number generator
+math.randomseed( os.time() )
+
+--No audio for now
+--audio.setVolume( 0.3 )
+-- Reserve channel 1 for background music
+--audio.reserveChannels( 1 )
+-- Reduce the overall volume of the channel
+--audio.setVolume( 0.1, { channel=1 } )
+
+-- Go to the menu screen
+composer.gotoScene( "menu" )
