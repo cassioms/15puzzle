5d032d0fd78f015d83e6b3841e97131b3f51ad8a
Cassio Matos <cms.usp@gmail.com>
2017-08-09 23:56:54 -0300

    Melhorando um pouco o design do game e icones do app

--- scripts/background.lua
+++ scripts/background.lua
@@ -1,26 +1,29 @@
-local background = {}
+local background = {
+    color = {1.0, 0.945, 0.3568}
+}
 
 function background.loadBackground(bgGroup)
     local rectangleHolder = display.newRect(bgGroup, display.contentCenterX, display.contentCenterY, 768, 1024)
-    display.setDefault("textureWrapX", "repeat")
-    display.setDefault("textureWrapY", "repeat")
+    rectangleHolder.fill = background.color
+    --display.setDefault("textureWrapX", "repeat")
+    --display.setDefault("textureWrapY", "repeat")
 
-    rectangleHolder.fill = {type = "image", filename = "design/bg_tile.png"}
+    --rectangleHolder.fill = {type = "image", filename = "design/bg_tile.png"}
 
-    local scaleFactorX = 1 ; local scaleFactorY = 1
-    if (rectangleHolder.width > rectangleHolder.height) then
-       scaleFactorY = rectangleHolder.width / rectangleHolder.height
-    else
-       scaleFactorX = rectangleHolder.height / rectangleHolder.width
-    end
+    --local scaleFactorX = 1 ; local scaleFactorY = 1
+    --if (rectangleHolder.width > rectangleHolder.height) then
+    --   scaleFactorY = rectangleHolder.width / rectangleHolder.height
+    --else
+    --   scaleFactorX = rectangleHolder.height / rectangleHolder.width
+    --end
 
-    rectangleHolder.fill.x = 0
-    rectangleHolder.fill.y = 0
-    rectangleHolder.fill.scaleX = 0.075 * scaleFactorX
-    rectangleHolder.fill.scaleY = 0.075 * scaleFactorY
+    --rectangleHolder.fill.x = 0
+    --rectangleHolder.fill.y = 0
+    --rectangleHolder.fill.scaleX = 0.075 * scaleFactorX
+    --rectangleHolder.fill.scaleY = 0.075 * scaleFactorY
 
-    display.setDefault("textureWrapX", "clampToEdge")
-    display.setDefault("textureWrapY", "clampToEdge")
+    --display.setDefault("textureWrapX", "clampToEdge")
+    --display.setDefault("textureWrapY", "clampToEdge")
 end
 
 return background
